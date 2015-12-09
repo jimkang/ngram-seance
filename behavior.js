3772dde250474eb9d56d621dfc7815d435856f98
Jim Kang <jimkang@gmail.com>
2015-12-09 09:39:40 -0500

    got rid of chiming in for now.

--- behavior.js
+++ behavior.js
@@ -2,19 +2,6 @@ var behavior = {
   hoursToWaitBetweenRepliesToSameUser: 1,
   hoursToWaitBetweenChimeIns: 4,
   chimeInUsers: [
-    'deathmtn',
-    'watchin_closely',
-    'gr8_note_taker',
-    'snakepeople_bot',
-    'hail_ants_bot',
-    'fuckingbard',
-    'autocompleterap',
-    'rapgamemetaphor',
-    'translatedbible',
-    'godtributes',
-    'deathmtn_eboox',
-    'wikisext',
-    'wrdsaladshootr'
   ]
 };
 
